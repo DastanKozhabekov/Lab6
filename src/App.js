ca531e69bd69d266e30c42add1ac237d3eebc87c
DastanKozhabekov <kozhabekovdastan@gmail.com>
2022-10-27 10:05:05 +0600

    ft

--- src/App.js
+++ src/App.js
@@ -1,12 +1,103 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import React from 'react';
 import './App.css';
+import {MDBFooter} from 'mdb-react-ui-kit';
+import {useState} from 'react'
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+ 
+  const reset = () =>{
+    setCounter(0)
+  };
+
+  const decrease = () => {
+    if (counter > 0) {
+      setCounter(count => count - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      fff
+    <>
+      {[false,].map((expand) => (
+        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">LAB 6</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <NavDropdown
+                    title="Dropdown"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#action3">Link</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Link
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+      <div className='container'>
+        <div className="counter">
+        <h1>React Counter</h1>
+        <span className="counter__output">{counter}</span>
+        <div className="btn__container">
+          <button className="control__btn" onClick={increase}>+</button>
+          <button className="control__btn" onClick={decrease}>-</button>
+          <button className="reset" onClick={reset}>Reset</button>
+      </div>
     </div>
+      </div>
+      <MDBFooter bgColor='light' className='text-center text-lg-left'>
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        &copy; {new Date().getFullYear()} Copyright:{' '}
+        <a className='text-dark' href='#'>
+          DS
+        </a>
+      </div>
+    </MDBFooter>
+    </>
   );
+  
 }
 
 export default App;
